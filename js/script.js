90e7496ff62f66fdb106dafd7318afa6cdd027ca
ibn2085 <yahyahridwan665@gmail.com>
2021-06-28 16:16:54 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function add(){
+    alert("it's a low Budget Calculator, the addition cant work");
+    // var a = prompt("enter ");
+    // var b = prompt("enter ");
+    // add = a + b;
+    // console.log(add);
+
+}
+function sub(){
+    var a = prompt("Enter your Number");
+    var b = prompt("Enter your Number");
+    add = a - b;
+    document.write(`${a} - ${b} = ${add}`);
+}
+function div(){
+    var a = prompt("Enter your Number");
+    var b = prompt("Enter your Number");
+    add = a / b;
+    document.write(`${a} / ${b} = ${add}`);
+}
+function mult(){
+    var a = prompt("Enter your Number");
+    var b = prompt("Enter your Number");
+    add = a * b;
+    document.write(`${a} * ${b} = ${add}`);
+}
